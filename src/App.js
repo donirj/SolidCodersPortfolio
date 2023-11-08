a18887d9b5171ed552e5bd5c588fa3927d4ebfef
xsorren <lauta.sorren@gmail.com>
2023-11-07 21:19:48 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner";
+import Header from "./components/Header";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
+import resumeData from './resumedata.js';
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Banner />
+      <Header resumeData={resumeData}/>
       <Skills />
       <Projects />
       <Contact />
