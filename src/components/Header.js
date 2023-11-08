bd991b1758ee7bc0159e555904de73a4e725b294
xsorren <lauta.sorren@gmail.com>
2023-11-07 21:26:09 -0300

    asd

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = (props)=>{
          <nav id="nav-wrap">
             <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
           <a className="mobile-btn" href="#" title="Hide navigation">Hide navigation</a>
-            <ul id="nav" className="nav">
+            <ul id="nav" className="nav" style={{justifyContent:"center"}}>
                <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
                <li><a className="smoothscroll" href="#about">About</a></li>
              <li><a className="smoothscroll" href="#resume">Resume</a></li>
