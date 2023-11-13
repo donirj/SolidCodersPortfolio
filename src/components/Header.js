4cfab8fb6e84493a585b948e5a6b19d15c28babc
xsorren <lauta.sorren@gmail.com>
2023-11-13 17:51:00 -0300

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -22,8 +22,8 @@ const Header = (props)=>{
 
          <div className="row banner">
             <div className="banner-text">
-               <h1 className="responsive-headline">I am {resumeData.name}.</h1>
-               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>I am a {resumeData.role}.{resumeData.roleDescription}
+               <h1 className="responsive-headline">{resumeData.name}.</h1>
+               <h3 style={{color:'#fff', fontFamily:'sans-serif '}}>Que onda doni, que fuente le podemos poner al titulo?{resumeData.role}.{resumeData.roleDescription}
                </h3>
                <hr/>
                <ul className="social">
